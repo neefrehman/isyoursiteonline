66799b3387a902144ab016e6e43686d37a840601
Neef Rehman <neef.rehman@gmail.com>
2018-11-15 10:43:31 +0000

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,39 @@
-$(".show-details").click(function() {
-    $(".details").show();
+$(".next1").click(function() {
+    $(".details1").hide();
+    $(".details2").show();
+    $(".next2").show();
+    $(this).hide();
+    return false;
+});
+
+$(".next2").click(function() {
+    $(".details2").hide();
+    $(".details3").show();
+    $(".next3").show();
+    $(this).hide();
+    return false;
+});
+
+$(".next3").click(function() {
+    $(".details3").hide();
+    $(".details4").show();
+    $(".next4").show();
+    $(this).hide();
+    return false;
+});
+
+$(".next4").click(function() {
+    $(".details4").hide();
+    $(".details5").show();
+    $(".next5").show();
+    $(this).hide();
+    return false;
+});
+
+$(".next5").click(function() {
+    $(".details5").hide();
+    $(".details1").show();
+    $(".next1").show();
     $(this).hide();
     return false;
 });
