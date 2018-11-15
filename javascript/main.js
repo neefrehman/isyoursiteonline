e56ac19e9b571386e0422e952922ce9bafbbff95
Neef Rehman <neef.rehman@gmail.com>
2018-11-15 10:59:49 +0000

    js tidy

--- javascript/main.js
+++ javascript/main.js
@@ -1,40 +1,40 @@
 $(".next1").click(function() {
     $(".details1").hide();
+    $(this).hide();
     $(".details2").show();
     $(".next2").show();
-    $(this).hide();
     return false;
 });
 
 $(".next2").click(function() {
     $(".details2").hide();
+    $(this).hide();
     $(".details3").show();
     $(".next3").show();
-    $(this).hide();
     return false;
 });
 
 $(".next3").click(function() {
     $(".details3").hide();
+    $(this).hide();
     $(".details4").show();
     $(".next4").show();
-    $(this).hide();
     return false;
 });
 
 $(".next4").click(function() {
     $(".details4").hide();
+    $(this).hide();
     $(".details5").show();
     $(".next5").show();
-    $(this).hide();
     return false;
 });
 
 $(".next5").click(function() {
     $(".details5").hide();
+    $(this).hide();
     $(".details1").show();
     $(".next1").show();
-    $(this).hide();
     return false;
 });
 
