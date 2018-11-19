778c70c751c4b3eb64812f48a7a6734cd5685d03
Myles Cowper-Coles <mylescc@gmail.com>
2018-11-19 21:20:29 +0000

    last bits

--- functions/sendgrid_confirm/index.js
+++ functions/sendgrid_confirm/index.js
@@ -1,14 +1,16 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-sgMail.setSubstitutionWrappers('{{', '}}');
 
 exports.handle= function(event, ctx, cb){
   const msg = {
     to: event.email,
-    from: 'no-reply@isyoursite.offline',
+    from: {
+      name: 'isyoursite.online',
+      email: 'no-reply@isyoursite.online',
+    },
     subject: 'your site is offline',
     templateId: 'd-9e95ebaf3cc04ce79b82423e57383c5e',
-    substitutions: {
+    dynamic_template_data: {
       confirm_email: event.confirm_url,
     },
   };
--- functions/sendgrid_offline/index.js
+++ functions/sendgrid_offline/index.js
@@ -1,20 +1,22 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-sgMail.setSubstitutionWrappers('{{', '}}');
 
-module.exports = function helperSendEmail(email, website){
+exports.handle = function(event, ctx, cb){
   const msg = {
-    to: email,
-    from: 'no-reply@isyoursite.offline',
+    to: event.email,
+    from: {
+      name: 'isyoursite.online',
+      email: 'no-reply@isyoursite.online',
+    },
     subject: 'your site is offline',
     templateId: 'd-6171b02881ea4115a088acdbafdba2f6',
-    substitutions: {
-      website_url: website,
+    dynamic_template_data: {
+      website_url: event.website,
     },
   };
   return sgMail.send(msg).then(function (response) {
-    console.log("sendgrid: then: " + JSON.stringify(response.data));
+    cb(null, response);
   }).catch((error) => {
-      console.log("sendgrid: catch: " + error);
+    cb(error);
   });
 };
