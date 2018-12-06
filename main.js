e190b69dccdb181ca336338c06f96c6b99440634
Neef Rehman <neef.rehman@gmail.com>
2018-12-06 22:23:52 +0000

    Remove bling

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-// bling.js
-
-window.$ = document.querySelectorAll.bind(document);
-
-Node.prototype.on = window.on = function (name, fn) {
-  this.addEventListener(name, fn);
-};
-
-NodeList.prototype.__proto__ = Array.prototype;
-
-NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
-  this.forEach(function (elem, i) {
-    elem.on(name, fn);
-  });
-};
-
-
 var descriptionArray = [
     { details: "Enter your email and website and we'll let you know if your site goes down",
       next: "The small print" },
@@ -32,24 +15,24 @@ var descriptionArray = [
 
 
 var descriptionState = 0;
-// var next = document.querySelector(".next");
-// var form = document.querySelector("Form");
-// var details = document.querySelector(".details");
-// var nextLink = document.querySelector(".next a");
+var next = document.querySelector(".next");
+var form = document.querySelector("Form");
+var details = document.querySelector(".details");
+var nextLink = document.querySelector(".next a");
 
 
-$(".next").on("click", function() {
+next.addEventListener("click", function() {
     descriptionState = descriptionState + 1;
     if (descriptionState > descriptionArray.length - 2) {
         descriptionState = 0;
     }
-    $(".details").innerHTML = descriptionArray[descriptionState].details;
-    $(".next a").innerHTML = descriptionArray[descriptionState].next;
+    details.innerHTML = descriptionArray[descriptionState].details;
+    nextLink.innerHTML = descriptionArray[descriptionState].next;
 });
 
 
 form.addEventListener("submit", function(e) {
-    $(".details").innerHTML = descriptionArray[descriptionArray.length - 1].details;
-    $(".next a").innerHTML = descriptionArray[descriptionArray.length - 1].next;
+    details.innerHTML = descriptionArray[descriptionArray.length - 1].details;
+    nextLink.innerHTML = descriptionArray[descriptionArray.length - 1].next;
     e.preventDefault();
 });
